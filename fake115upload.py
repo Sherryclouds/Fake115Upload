f632ef1e0a3f8b503fe6cfdba071be105a476efd
T3rry7f <731531775@qq.com>
2019-05-08 17:34:52 +0800

    Update fake115upload.py

--- fake115upload.py
+++ fake115upload.py
@@ -1,15 +1,14 @@
-
 __author__ = 'T3rry'
 
-import ctypes,os,sys
-import urllib2, urllib, cookielib
+import os,sys
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder 
 import json
-############################################################################### Need your cookie
+import hashlib
+#############################################################  Need your cookie
 COOKIES={}
-COOKIESTEXT="115_lang=zh; OOFL=1234; UID=12304_A1_1556868230; CID=123c9efe13fa29b5eba87c56c39418; SEID=e1233b1514f615772f4f690a29885bbe6677f2bea2827eb0e129d797703b45c09445367f81e65be9ba475cae9a5518cb3b6e62dc5ae4541166d7684; last_video_volume=100"
-############################################################################### Need your cookie
+COOKIESTEXT="115_lang=zh; OOFL=51051904; UID=51051904_A1_1556868230; CID=73c21c9efe13fa29b5eba87c56c39418; SEID=e04613b1514f615772f4f690a29885bbe6677f2bea2827eb0e129d797703b45c09445367f81e65be9ba475cae9a5518cb3b6e62dc5ae4541166d7684; last_video_volume=100"
+#############################################################  Need your cookie
 user_id=""
 userkey=""
 target="U_1_0"
@@ -18,138 +17,7 @@ app_ver='11.2.0'
 pickcode=""
 header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
 linksfile="115links.txt"
-################################################################################
-def int_overflow(val):
-    maxint = 2147483647
-    if not -maxint-1 <= val <= maxint:
-        val = (val + (maxint + 1)) % (2 * (maxint + 1)) - maxint - 1
-    return val
-
-def unsigned_right_shitf(n,i):
-    if n<0:
-        n = ctypes.c_uint32(n).value
-    if i<0:
-        return -int_overflow(n << abs(i))
-    return int_overflow(n>>i)
-
-def  build_hash(a):
-	v0 = "0123456789abcdef"
-	v1 = ""
-	v2=0
-	for i in range(0,len(a)*4):
-		v3 = v2 >> 2
-		v5 = (3 - v2 % 4) * 8;
-		v4 = v0[(a[v3] >> v5 + 4 & 15)]
-		v3_1 = v0[(a[v3]>> v5 & 15)]
-		v1 = v1 + v4 + v3_1
-		v2+=1
-	return v1
-   
-def dha(a):
-
-	v0 =[0]*len(a)*8
-	for i in range(0,len(v0),8):
-		v1=i>>5
-		v0[v1]|=(ord(a[i/8])&255)<<24-i%32
-	i_v3=0
-	for i in v0:
-		if i!=0:
-			i_v3+=1
-	v3=[0]*i_v3
-	for j in range(0,len(v3)):
-		v3[j]=v0[j]
-	return v3
-
-def dhaa(a,b):
-	v1=b>>5
-	v2=dhbb(a,v1)
-	v2[v1]|=128<<24-b%32
-	v1=(b+64>>9<<4)+15
-	v2=dhbb(v2,v1)
-	v2[v1]=b
-	v0=80
-	v1_1=[0]*80
-	v5=0
-	v6=0x67452301
-	v7=-0x10325477
-	v8=-0x67452302
-	v9=0x10325476
-	v10=-0x3c2d1e10
-	while(True):
-		v12=1
-		v13=5
-		if (v5 >=len(v2)):
-			break
-		v14=v6
-		v15=v7
-		v3=v8
-		v4=v9
-		v16=v10
-		v11=0
-		while (v11<v0):
-			v1_1[v11]=v2[v5+v11] if v11<16 else dhc(v1_1[v11-3]^v1_1[v11-8]^v1_1[v11-14]^v1_1[v11-16],v12)
-			v0=dhb(dhb(dhc(v14,v13),dhd(v11,v15,v3,v4)),dhb(dhb(v16,v1_1[v11]),dhe(v11)))
-			v12=dhc(v15,30)
-			v11+=1
-			v16=v4
-			v15=v14
-			v13=5
-			v14=v0
-			v4=v3
-			v3=v12
-			v0=80
-			v12=1
-		v6=dhb(v14,v6)
-		v7=dhb(v15,v7)
-		v8=dhb(v3,v8)
-		v9=dhb(v4,v9)
-		v10=dhb(v16,v10)
-		v5+=16
-		v0=80
-	ret=[v6,v7,v8,v9,v10]
-	return ret
-
-def dhb(ia,ib):
-	v1=int_overflow(ia&65535)+(ib&65535)
-	return int_overflow(int_overflow(int_overflow(ia>>16)+int_overflow(ib>>16)+int_overflow(v1>>16)<<16)|v1&65535)
-
-def dhbb(a,b):
-	v0 = len(a)
-	v1 = b + 1;
-	if(v0 >= v1):
-		return a    
-	v1_1 = [0]*v1
-	v2 = 0   
-	while(v2 < v0) :
-		v1_1[v2] = a[v2];
-		v2+=1
-	return v1_1
-
-def dhc(a,b):
-	return int_overflow(int_overflow(unsigned_right_shitf(a,32-b)) | int_overflow(a << b))
-
-def dhd(a,b,c,d):
-	if a<20:
-		return b&c|(b^-1)&d
-	if a<40:
-		return b^c^d
-	if a<60:
-		return b&c|b&d|c&d
-	return b^c^d
-
-def dhe(a):
-	if a<20:
-		a=0x5a827999
-	elif a<40:
-		a=0x6ed9eba1
-	elif a<60:
-		a=-0x70e44324
-	else:
-		a=-0x359d3e2a
-	return a
-
-def get_signature (s):
-	return build_hash(dhaa(dha(s), len(s) * 8))
+##########################################################
 
 ############################################################################################################
 def Upload_files_by_sha1_from_links(filename):  #link sample : 1.mp4|26984894148|21AEB458C98643D5E5E4374C9D2ABFAAA4C6DA6
@@ -162,9 +30,9 @@ def Upload_files_by_sha1_from_links(filename):  #link sample : 1.mp4|26984894148
 		if(len(fileid)!=40):
 			print 'Error links'
 		quickid=fileid
-		hash=get_signature(user_id+fileid+quickid+pickcode+target+'0')
+		hash=hashlib.sha1(user_id+fileid+quickid+pickcode+target+'0').hexdigest()
 		a=userkey+hash+end_string
-		sig=get_signature(a).upper()
+		sig=hashlib.sha1(a).hexdigest().upper()
 		URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
 		header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
 		postData={
@@ -203,9 +71,11 @@ def Upload_file_by_sha1(fileid,filesize,filename):  #quick
 	GetUserKey()
 	fileid=fileid.upper()
 	quickid=fileid
-	hash=get_signature(user_id+fileid+quickid+pickcode+target+'0')
+	hash=hashlib.sha1((user_id+fileid+quickid+pickcode+target+'0')).hexdigest()
 	a=userkey+hash+end_string
-	sig=get_signature(a).upper()
+	print a
+	sig=hashlib.sha1(a).hexdigest().upper()
+	print sig
 	URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
 	postData={
 				'preid':'',
@@ -219,8 +89,8 @@ def Upload_file_by_sha1(fileid,filesize,filename):  #quick
 				'target':target,
 				'fileid':fileid
 			  }
-	r = requests.post(URL, data=postData,headers=header)
-	print(r.text)
+	#r = requests.post(URL, data=postData,headers=header)
+	#print(r.text)
 
 def Upload_file_from_local(filename):  #slow
 	uri='http://uplb.115.com/3.0/sampleinitupload.php'
@@ -251,5 +121,6 @@ if __name__ == '__main__':
 		filename=sys.argv[1]
 		Upload_file_from_local(filename)
 	else:
+		pass
 		Upload_files_by_sha1_from_links(linksfile)
 		
