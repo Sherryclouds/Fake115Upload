7747d93d0fdcd42a40488fa934463504446aa98c
T3rry7f <731531775@qq.com>
2019-05-08 13:27:47 +0800

    Update fake115upload.py
    支持本地上传和极速秒传

--- fake115upload.py
+++ fake115upload.py
@@ -1,20 +1,21 @@
-import ctypes
-import numpy
+import ctypes,os,sys
 import urllib2, urllib, cookielib
 import requests
-
+from requests_toolbelt.multipart.encoder import MultipartEncoder 
+import json
 #############################################################
-user_id="342296031"
-userkey="61CC6FA7318AAD4A0BAC73DC9D7DF7446AC9E3E8".upper()
+user_id="51051904"
+userkey="0CBCB81CA63E48F7BF7650D85863562CC41E37B9".upper()
+COOKIES={}
+COOKIESTEXT="115_lang=zh; OOFL=51051904; UID=51051904_A1_1556868230; CID=73c21c9efe13fa29b5eba87c56c39418; SEID=e04613b1514f615772f4f690a29885bbe6677f2bea2827eb0e129d797703b45c09445367f81e65be9ba475cae9a5518cb3b6e62dc5ae4541166d7684; last_video_volume=100"
+
 target="U_1_0"
 end_string="000000"
 app_ver='11.2.0'
 pickcode=""
-fileid="DB3D9A85D248312FE16C3B2AC12BD0D1E416ED79".upper()
-quickid=fileid
-filesize='2347218627'
-filename="1Flames.of.Desire.EP09.720p.HDTV.x264-NGB.mkv"
-#115code={}
+header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
+linksfile="115links.txt"
+
 ##############################################################
 def int_overflow(val):
     maxint = 2147483647
@@ -148,19 +149,66 @@ def dhe(a):
 def get_signature (s):
 	return build_hash(dhaa(dha(s), len(s) * 8))
 
-hash=get_signature(user_id+fileid+quickid+pickcode+target+'0')
-a=userkey+hash+end_string
-sig=get_signature(a).upper()
+
+
 #print "Signature:",sig
 
 ############################################################################################################
-def read_115link_from_txt():
-	f=open('115link.txt','r')
-	f.close()
+def Upload_files_by_sha1_from_links(filename):  #link sample : 1.mp4|26984894148|21AEB458C98643D5E5E4374C9D2ABFAAA4C6DA6
 
-def get_file_by_sha1(fileid,filesize,filename):
+	read_userkey_from_ini('config.ini')
+	for l in open(filename,'r'):
+		link=l.split('|')
+		filename=link[0]
+		filesize=link[1]
+		fileid=link[2]
+		if(len(fileid)!=40):
+			print 'Error links'
+		quickid=fileid
+		hash=get_signature(user_id+fileid+quickid+pickcode+target+'0')
+		a=userkey+hash+end_string
+		sig=get_signature(a).upper()
+		URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
+		header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
+		postData={
+				'preid':'',
+				'filename':filename,
+				'quickid':fileid,
+				'user_id':user_id,
+				'app_ver':app_ver,
+				'filesize':filesize,
+				'userid':user_id,
+				'exif':'',
+				'target':target,
+				'fileid':fileid
+			  }
+		r = requests.post(URL, data=postData,headers=header)
+		print(r.text)
+
+def read_userkey_from_ini(inifile):
+	global user_id,userkey
+	if os.path.exists(inifile):
+		for l in open(inifile,'r'):
+			info=l.split('=')
+			if(info[0]=='user_id'):user_id=info[1].strip()
+			if(info[0]=='userkey'):userkey=info[1].strip().upper()
+
+def GetFileSize(file):
+	return os.path.getsize(file)
+
+def AddCookie(cook):
+	for line in COOKIESTEXT.split(';'):   
+		name,value=line.strip().split('=',1)  
+		COOKIES[name]=value 
+
+
+def Upload_file_by_sha1(fileid,filesize,filename):  #quick
+	fileid=fileid.upper()
+	quickid=fileid
+	hash=get_signature(user_id+fileid+quickid+pickcode+target+'0')
+	a=userkey+hash+end_string
+	sig=get_signature(a).upper()
 	URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
-	header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
 	postData={
 				'preid':'',
 				'filename':filename,
@@ -176,5 +224,36 @@ def get_file_by_sha1(fileid,filesize,filename):
 	r = requests.post(URL, data=postData,headers=header)
 	print(r.text)
 
-get_file_by_sha1(fileid,filesize,filename)
+def Upload_file_from_local(filename):  #slow
+	uri='http://uplb.115.com/3.0/sampleinitupload.php'
+	AddCookie(COOKIESTEXT)
+	postdata={"userid":user_id,"filename":filename,"filesize":GetFileSize(filename),"target":target}
+	r = requests.post(uri,headers=header,cookies=COOKIES,data=postdata)
+	resp=json.loads(r.content) 
+	print resp
+	req_headers = {'Content-Type': "multipart/form-data; boundary=----WebKitFormBoundarya2JSh7swYU46OdJ0"}
+	m = MultipartEncoder(fields=[('name', filename), 
+                             ('key', resp['object']),
+                             ('policy',resp['policy']),
+                             ('OSSAccessKeyId', resp['accessid']),
+                             ('success_action_status', '200'),
+                             ( 'callback',resp['callback']),
+                             ('signature',resp['signature']),
+                             ('file',(filename,open(filename, 'rb'), 'video/mp4'))],
+                     		boundary='----WebKitFormBoundarya2JSh7swYU46OdJ0'
+                    )
+	r = requests.post(resp['host'],headers=req_headers,data=m)
 
+	print r.content
+
+
+print "Author @T3rry \n"
+#Upload_files_by_sha1_from_links('links.txt')
+#Upload_file_by_sha1('321AEB458C98643D5E5E4374C9D2ABFAAA4C6DA6','26984894148','1.mp4')
+#Upload_file_from_local("1.mp4")
+if __name__ == '__main__':
+	if len(sys.argv)>1:
+		filename=sys.argv[1]
+		Upload_file_from_local(filename)
+	else:
+		Upload_files_by_sha1_from_links(linksfile)
