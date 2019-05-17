d00d9d531e4e05e3a09e84d9edb6888cd54a08c8
T3rry7f <731531775@qq.com>
2019-05-17 21:08:57 +0800

    Update fake115upload.py

--- fake115upload.py
+++ fake115upload.py
@@ -7,7 +7,7 @@ import json
 import hashlib
 #############################################################  Need your cookie
 COOKIES={}
-COOKIESTEXT="115_lang=zh; OOFL=51051904; UID=51051904_A1_1556868230; CID=73c21c9efe13fa29b5eba87c56c39418; SEID=e04613b1514f615772f4f690a29885bbe6677f2bea2827eb0e129d797703b45c09445367f81e65be9ba475cae9a5518cb3b6e62dc5ae4541166d7684; last_video_volume=100"
+COOKIESTEXT="acw_tc=784e2c9b15559381746522938e543029c6467c65f6521ffcf8ffe29140d0bd; last_video_volume=100; UID=123455; CID=1231231231; SEID=123123; 115_lang=zh"
 #############################################################  Need your cookie
 user_id=""
 userkey=""
@@ -17,36 +17,20 @@ app_ver='11.2.0'
 pickcode=""
 header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
 linksfile="115links.txt"
-#######################################################################
-def Upload_files_by_sha1_from_links(filename):  #link sample : 1.mp4|26984894148|21AEB458C98643D5E5E4374C9D2ABFAAA4C6DA6
+##########################################################
+
+############################################################################################################
+def Upload_files_by_sha1_from_links(file):  #link sample : 1.mp4|26984894148|21AEB458C98643D5E5E4374C9D2ABFAAA4C6DA6
 	GetUserKey()
-	for l in open(filename,'r'):
+	for l in open(file,'r'):
 		link=l.split('|')
 		filename=link[0]
 		filesize=link[1]
 		fileid=link[2]
 		if(len(fileid)!=40):
 			print 'Error links'
-		quickid=fileid
-		hash=hashlib.sha1(user_id+fileid+quickid+pickcode+target+'0').hexdigest()
-		a=userkey+hash+end_string
-		sig=hashlib.sha1(a).hexdigest().upper()
-		URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
-		header = { "User-Agent" : 'Mozilla/5.0  115disk/11.2.0'}
-		postData={
-				'preid':'',
-				'filename':filename,
-				'quickid':fileid,
-				'user_id':user_id,
-				'app_ver':app_ver,
-				'filesize':filesize,
-				'userid':user_id,
-				'exif':'',
-				'target':target,
-				'fileid':fileid
-			  }
-		r = requests.post(URL, data=postData,headers=header)
-		print(r.text)
+			return
+		Upload_file_by_sha1(fileid,filesize,filename)
 
 
 def GetFileSize(file):
@@ -71,9 +55,7 @@ def Upload_file_by_sha1(fileid,filesize,filename):  #quick
 	quickid=fileid
 	hash=hashlib.sha1((user_id+fileid+quickid+pickcode+target+'0')).hexdigest()
 	a=userkey+hash+end_string
-	print a
 	sig=hashlib.sha1(a).hexdigest().upper()
-	print sig
 	URL="http://uplb.115.com/3.0/initupload.php?isp=0&appid=0&appversion=11.2.0&format=json&sig="+sig
 	postData={
 				'preid':'',
@@ -119,5 +101,6 @@ if __name__ == '__main__':
 		filename=sys.argv[1]
 		Upload_file_from_local(filename)
 	else:
+		pass
 		Upload_files_by_sha1_from_links(linksfile)
 		
